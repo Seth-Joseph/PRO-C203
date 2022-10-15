61225e7b4310e204f52595e56a0843b3e335ff04
Seth-Joseph <61108580+Seth-Joseph@users.noreply.github.com>
2022-10-15 21:57:18 +0530

    Update client.py

--- client.py
+++ client.py
@@ -2,14 +2,19 @@ import socket
 from threading import Thread
 from tkinter import *
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 ip_address = '127.0.0.1'
-port =8000
-client.connect((ip_address,port))
-print('Client connected.')
+port = 8000
+
+client.connect((ip_address, port))
+
+print("Connected to the server successfully!")
+
 
 class GUI:
     def __init__(self):
+
         self.window = Tk()
         self.window.withdraw()
         
@@ -32,31 +37,26 @@ class GUI:
         self.loginBtn.place(anchor=CENTER,relx=0.53,rely=0.6)
         self.window.mainloop()
 
-    def goAhead(self,name):
+    def goAhead(self, name):
         self.login.destroy()
         self.layout(name)
-        rcv = Thread(target=self.recieve)
+        rcv = Thread(target=self.receive)
         rcv.start()
-    
-    
-    def layout(self,name):
+
+    def layout(self, name):
+
         self.name = name
         self.window.deiconify()
-        self.window.title('Chat room')
+        self.window.title("CHATROOM")
         self.window.resizable(width=False,height=False)
         self.window.configure(width=775,height=550,bg='#202121')
 
-        self.labelHead = Label(text=self.name,bg='#202121',fg='#FFF',font='Helvetica 18 bold',pady=5)
+        self.labelHead = Label(self.window,text=self.name,bg='#202121',fg='#FFF',font='Helvetica 18 bold',pady=5)
         self.labelHead.place(relx=0.5,rely=0.02,anchor=CENTER)
 
-        self.textCons = Text(self.window,width = 20,height = 2,bg = "#252525",fg = "#EAECEE",font = "Helvetica 14",padx = 5,pady = 5,bd=0,highlightthickness=2)
+        self.textCons = Text(self.window,width = 20,height = 2,bg = "#252525",fg = "#FFF",font = "Helvetica 14",padx = 5,pady = 5,bd=0,highlightthickness=2)
         self.textCons.place(relheight = 0.745,relwidth = 0.95,rely = 0.06,relx=0.025)
         self.textCons.config(cursor='arrow',highlightbackground = "black", highlightcolor= "black")
-
-        self.scrollBar = Scrollbar(self.textCons)
-        self.scrollBar.place(relx=0.98,relheight=1)
-        self.scrollBar.config(command=self.textCons.yview)
-
 
         self.labelBottom = Label(self.window,bg='#202121',height=80)
         self.labelBottom.place(relwidth = 1,rely = 0.82)
@@ -66,35 +66,41 @@ class GUI:
         self.entryMsg.place(relx=0.045,rely=0.02,relheight=0.04,relwidth=0.8)
         self.entryMsg.focus()
 
-        self.sendBtn = Button(self.labelBottom,text="Send",font='Helvetica 14 bold',width=30,bg='#00a985',fg='#FFF',bd=0,command=lambda: self.sendButton(self.entryMsg.get()))
-        self.sendBtn.place(relx=0.855,rely=0.02,relheight=0.04,relwidth=0.1)
+        self.buttonMsg = Button(self.labelBottom,text="Send",font='Helvetica 14 bold',width=30,bg='#00a985',fg='#FFF',bd=0,command=lambda: self.sendButton(self.entryMsg.get()))
+        self.buttonMsg.place(relx=0.855,rely=0.02,relheight=0.04,relwidth=0.1)
+
+
+        scrollbar = Scrollbar(self.textCons)
+        scrollbar.place(relheight=1,relx=0.974)
+        scrollbar.config(command=self.textCons.yview)
+
+        self.textCons.config(state=DISABLED)
 
     def sendButton(self, msg):
-        self.textCons.config(state = DISABLED)
-        self.msg=msg
+        self.textCons.config(state=DISABLED)
+        self.msg = msg
         self.entryMsg.delete(0, END)
-        snd= Thread(target = self.write)
+        snd = Thread(target=self.write)
         snd.start()
 
-    def show_message(self,message):
+    def show_message(self, message):
         self.textCons.config(state=NORMAL)
-        self.textCons.insert(END,message+"\n\n")
+        self.textCons.insert(END, message+"\n\n")
         self.textCons.config(state=DISABLED)
         self.textCons.see(END)
 
-    def recieve(self):
+    def receive(self):
         while True:
             try:
-                msg = client.recv(2048).decode('utf-8')
-                if msg == 'NICKNAME':
+                message = client.recv(2048).decode('utf-8')
+                if message == 'NICKNAME':
                     client.send(self.name.encode('utf-8'))
                 else:
                     self.show_message(message)
             except:
-                print('Connection error.')
+                print("An error occured!")
                 client.close()
                 break
-
 
     def write(self):
         self.textCons.config(state=DISABLED)
@@ -104,4 +110,5 @@ class GUI:
             self.show_message(message)
             break
 
-g1 = GUI()
+
+g = GUI()
